08b045f8a21e9937fd2a1af1a7cb659377f87011
Petri Weckström <petriweckstrom@Petris-MBP.home>
2023-09-04 20:49:35 +0300

    Launch of RobotSpareBin site and close annoying modal

--- tasks.py
+++ tasks.py
@@ -1,6 +1,5 @@
 from robocorp.tasks import task
 from robocorp import browser, http, excel
-
 from RPA.Tables import Tables
 
 
@@ -18,12 +17,27 @@ def order_robots_from_RobotSpareBin():
     )
 
     open_robot_order_website()
-    download_oreders_excel_file()
-
+    # download_oreders_excel_file()
+    orders = get_orders()
+    for row in orders:
+        print(row)
+        fill_the_form(row)
+    
 def open_robot_order_website():
     """Opens RobotSpareBin Industries Inc website for Robot orders"""
     page = browser.goto("https://robotsparebinindustries.com/#/robot-order")
         
-def download_oreders_excel_file():
+def get_orders():
     """Download the orders CSV file, read it as a table, and return the result"""
     http.download(url="https://robotsparebinindustries.com/orders.csv", overwrite=True)
+    # Create new empty table and read order rows from downloaded csv
+    orders = Tables()
+    order_rows = orders.read_table_from_csv(path="orders.csv")
+    return order_rows
+
+def fill_the_form(row):
+    close_annoying_modal()
+
+def close_annoying_modal():
+    page = browser.page()
+    page.click("button:text('Yep')")
